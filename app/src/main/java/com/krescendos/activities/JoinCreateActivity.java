17af354a4d9b05e1ce2004f117e56fd549a05347
Mungrel <nick.pollett@outlook.com>
2017-08-10 21:16:46 +1200

    Finally got the title bar styling to work

--- app/src/main/java/com/krescendos/activities/JoinCreateActivity.java
+++ app/src/main/java/com/krescendos/activities/JoinCreateActivity.java
@@ -3,6 +3,7 @@ package com.krescendos.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,8 @@ public class JoinCreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_create);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Button joinButton = (Button) findViewById(R.id.joinButton);
         Button createButton = (Button) findViewById(R.id.createButton);
