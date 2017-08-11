b0c1ac6c2edac604d7f242d263863e35e0b432c1
Mungrel <nick.pollett@outlook.com>
2017-08-11 17:23:29 +1200

    Join activity styled

--- app/src/main/java/com/krescendos/activities/JoinActivity.java
+++ app/src/main/java/com/krescendos/activities/JoinActivity.java
@@ -2,10 +2,10 @@ package com.krescendos.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.krescendos.R;
+import com.krescendos.input.TextChangeListener;
 import com.krescendos.web.Requester;
 
 import org.json.JSONObject;
@@ -34,13 +35,27 @@ public class JoinActivity extends AppCompatActivity {
         }
 
         Button joinCodeSubmit = (Button) findViewById(R.id.joinCodeSubmitButton);
-        final EditText joinCodeTextField = (EditText) findViewById(R.id.joinCodeField);
+        final EditText text1 = (EditText) findViewById(R.id.joinCode1);
+        final EditText text2 = (EditText) findViewById(R.id.joinCode2);
+        final EditText text3 = (EditText) findViewById(R.id.joinCode3);
+        final EditText text4 = (EditText) findViewById(R.id.joinCode4);
+        final EditText text5 = (EditText) findViewById(R.id.joinCode5);
+        final EditText text6 = (EditText) findViewById(R.id.joinCode6);
+
+        text1.addTextChangedListener(new TextChangeListener(text1, text2));
+        text2.addTextChangedListener(new TextChangeListener(text1, text3));
+        text3.addTextChangedListener(new TextChangeListener(text2, text4));
+        text4.addTextChangedListener(new TextChangeListener(text3, text5));
+        text5.addTextChangedListener(new TextChangeListener(text4, text6));
+        text6.addTextChangedListener(new TextChangeListener(text5, text6));
+
         final TextView errorText = (TextView) findViewById(R.id.joinErrorTextView);
 
         joinCodeSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String text = joinCodeTextField.getText().toString();
+                String text = text1.getText().toString();
+                text += text2.getText().toString();
                 errorText.setVisibility(View.INVISIBLE);
                 requester.join(text, new Response.Listener<JSONObject>() {
                     @Override
@@ -70,5 +85,11 @@ public class JoinActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        Log.d("ONKEYUP", "");
+        return super.onKeyUp(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/krescendos/input/TextChangeListener.java
@@ -0,0 +1,34 @@
+package com.krescendos.input;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class TextChangeListener implements TextWatcher {
+
+    private EditText left;
+    private EditText right;
+
+    public TextChangeListener(EditText left, EditText right){
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if (!editable.toString().isEmpty()){
+            right.requestFocus();
+        } else {
+            left.requestFocus();
+        }
+    }
+}
