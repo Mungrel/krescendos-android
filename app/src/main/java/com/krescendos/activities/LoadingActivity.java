78d7a217e5653b10bc2bc148dc26ecefe680900b
Mungrel <nick.pollett@outlook.com>
2017-08-18 11:09:46 +1200

    Made loading activity the starting activity

--- app/src/main/java/com/krescendos/activities/LoadingActivity.java
+++ app/src/main/java/com/krescendos/activities/LoadingActivity.java
@@ -1,5 +1,6 @@
 package com.krescendos.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,23 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        Thread splashThread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(getApplicationContext(), JoinCreateActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        splashThread.start();
     }
 }
