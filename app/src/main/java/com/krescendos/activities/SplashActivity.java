b18851dbd374913031831b40a2fdd71abe55413b
Mungrel <nick.pollett@outlook.com>
2017-08-20 14:43:54 +1200

    Add splash pause logic

--- app/src/main/java/com/krescendos/activities/SplashActivity.java
+++ app/src/main/java/com/krescendos/activities/SplashActivity.java
@@ -1,15 +1,34 @@
 package com.krescendos.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.krescendos.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private static long SPLASH_TIME_MS = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread splashPause = new Thread(){
+          @Override
+            public void run(){
+              try {
+                  sleep(SPLASH_TIME_MS);
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              } finally {
+                  Intent intent = new Intent(getApplicationContext(), JoinCreateActivity.class);
+                  intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                  startActivity(intent);
+              }
+          }
+        };
+        
+        splashPause.start();
     }
 }
