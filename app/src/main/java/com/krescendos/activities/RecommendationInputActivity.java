f3772b19e8c4096566f67d4eeb5436cfa7c2aca1
Mungrel <nick.pollett@outlook.com>
2017-08-21 17:08:52 +1200

    Add Recommendation input basic empty activity

---
+++ app/src/main/java/com/krescendos/activities/RecommendationInputActivity.java
@@ -0,0 +1,13 @@
+package com.krescendos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecommendationInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendation_input);
+    }
+}
