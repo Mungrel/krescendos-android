29da9a3bfc6ad2f82390adef41843143f33169a4
Mungrel <nick.pollett@outlook.com>
2017-08-28 11:20:16 +1200

    Create UpdateTimer object and interface

---
+++ app/src/main/java/com/krescendos/timer/OnTimerUpdateListener.java
@@ -0,0 +1,6 @@
+package com.krescendos.timer;
+
+public interface OnTimerUpdateListener {
+
+    public void onUpdate(long time);
+}
---
+++ app/src/main/java/com/krescendos/timer/UpdateTimer.java
@@ -0,0 +1,41 @@
+package com.krescendos.timer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class UpdateTimer {
+
+    private static int REPEAT_TIME_MS = 200;
+
+    private Timer timer;
+    private long time;
+    private OnTimerUpdateListener onTimerUpdateListener;
+
+    public UpdateTimer(final OnTimerUpdateListener onTimerUpdateListener){
+        this.onTimerUpdateListener = onTimerUpdateListener;
+        time = 0;
+        timer = new Timer();
+    }
+
+    public void start(){
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                time += REPEAT_TIME_MS;
+                onTimerUpdateListener.onUpdate(time);
+            }
+        }, 0, REPEAT_TIME_MS);
+    }
+
+    public void pause(){
+        timer.cancel();
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time){
+        this.time = time;
+    }
+}
