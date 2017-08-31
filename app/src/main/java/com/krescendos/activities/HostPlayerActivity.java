7d6f830f339b1fa6a7e7def4e0d72f410d743b77
Nick <npol807@aucklanduni.ac.nz>
2017-08-31 14:00:52 +1200

    Bug fix/client request state update on init (#19)
    * Add state update request listener to host
    
    * Integrate new endpoint for update request
    
    * Change endpoint request method
    
    * Make seekbar more stable when dragging

--- app/src/main/java/com/krescendos/activities/HostPlayerActivity.java
+++ app/src/main/java/com/krescendos/activities/HostPlayerActivity.java
@@ -33,6 +33,7 @@ import com.krescendos.text.TextUtils;
 import com.krescendos.text.Time;
 import com.krescendos.web.PlaylistChangeListener;
 import com.krescendos.web.Requester;
+import com.krescendos.web.StateUpdateRequestListener;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
@@ -161,7 +162,7 @@ public class HostPlayerActivity extends AppCompatActivity implements ConnectionS
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if (mPlayer != null) {
+                        if (mPlayer != null && !mPlayer.isDragging()) {
                             seekBar.setMax((int) mPlayer.getCurrentTrackLength());
                             seekBar.setProgress((int) mPlayer.getCurrentTrackTime());
                             long remaining = mPlayer.getCurrentTrackLength() - mPlayer.getCurrentTrackTime();
@@ -206,6 +207,7 @@ public class HostPlayerActivity extends AppCompatActivity implements ConnectionS
                             mPlayer = new TrackPlayer(spotifyPlayer, getApplicationContext(), party.getPartyId());
                             seekBar.setOnSeekBarChangeListener(new SeekBarUserChangeListener(mPlayer));
                             ref.child("playlist").addValueEventListener(new PlaylistChangeListener(listAdapter, mPlayer));
+                            ref.child("partyStateUpdateRequested").addValueEventListener(new StateUpdateRequestListener(getApplicationContext(), party.getPartyId(), mPlayer));
                             mPlayer.setOnTrackChangeListener(new OnTrackChangeListener() {
                                 @Override
                                 public void onTrackChange(Track newTrack) {
---
+++ app/src/main/java/com/krescendos/web/StateUpdateRequestListener.java
@@ -0,0 +1,29 @@
+package com.krescendos.web;
+
+import android.content.Context;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.krescendos.player.TrackPlayer;
+
+public class StateUpdateRequestListener implements ValueEventListener {
+    private TrackPlayer mPlayer;
+    private Requester requester;
+    private String partyCode;
+
+    public StateUpdateRequestListener(Context context, String partyCode, TrackPlayer mPlayer) {
+        this.mPlayer = mPlayer;
+        this.partyCode = partyCode;
+        this.requester = Requester.getInstance(context);
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        requester.updatePlayState(partyCode, mPlayer.getState());
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+    }
+}
