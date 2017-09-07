97b30c409dfec888d92a2b8ba52e4c1089a9b0ba
Mungrel <nick.pollett@outlook.com>
2017-09-07 13:23:17 +1200

    Add error object

---
+++ app/src/main/java/com/krescendos/domain/Error.java
@@ -0,0 +1,32 @@
+package com.krescendos.domain;
+
+public class Error {
+
+    private int status;
+    private String message;
+    private String userMessage;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUserMessage() {
+        return userMessage;
+    }
+
+    public void setUserMessage(String userMessage) {
+        this.userMessage = userMessage;
+    }
+}
