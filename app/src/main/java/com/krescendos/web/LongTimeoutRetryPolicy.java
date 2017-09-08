8662402ca414041eb284edb12c9b3949b1676d5c
Mungrel <nick.pollett@outlook.com>
2017-09-08 13:43:49 +1200

    Remove extra log statements

--- app/src/main/java/com/krescendos/web/LongTimeoutRetryPolicy.java
+++ app/src/main/java/com/krescendos/web/LongTimeoutRetryPolicy.java
@@ -2,12 +2,12 @@ package com.krescendos.web;
 
 import com.android.volley.DefaultRetryPolicy;
 
-public class LongTimeoutRetryPolicy extends DefaultRetryPolicy {
+class LongTimeoutRetryPolicy extends DefaultRetryPolicy {
 
     private static final int TIMEOUT_MS = 3000;
     private static final int RETRY_ATTEMPTS = 3;
 
-    public LongTimeoutRetryPolicy() {
+    LongTimeoutRetryPolicy() {
         super(TIMEOUT_MS, RETRY_ATTEMPTS, DEFAULT_BACKOFF_MULT);
     }
 }
