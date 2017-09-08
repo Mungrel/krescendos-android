4708408c734412ee0c42c88f6b99820518976ba4
Mungrel <nick.pollett@outlook.com>
2017-09-08 13:52:45 +1200

    Remove unused vars and parameters

--- app/src/main/java/com/krescendos/web/PlayheadIndexChangeListener.java
+++ app/src/main/java/com/krescendos/web/PlayheadIndexChangeListener.java
@@ -1,49 +1,27 @@
 package com.krescendos.web;
 
-import android.content.Context;
 import android.util.Log;
-import android.widget.LinearLayout;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.NetworkImageView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
-import com.krescendos.R;
 import com.krescendos.domain.Track;
 import com.krescendos.player.TrackListAdapter;
-import com.krescendos.text.TextUtils;
 
 public class PlayheadIndexChangeListener implements ValueEventListener {
 
     private TrackListAdapter adapter;
-    private ImageLoader imageLoader;
-
-    private TextView trackTitle;
-    private TextView artistAlbum;
-    private NetworkImageView albumArt;
     private SeekBar seekBar;
 
-    public PlayheadIndexChangeListener(Context context, LinearLayout trackDetailsLayout, TrackListAdapter adapter, SeekBar seekBar) {
+    public PlayheadIndexChangeListener(TrackListAdapter adapter, SeekBar seekBar) {
         this.adapter = adapter;
-        this.imageLoader = new Requester(context).getImageLoader();
         this.seekBar = seekBar;
-
-        trackTitle = trackDetailsLayout.findViewById(R.id.current_track_title);
-        artistAlbum = trackDetailsLayout.findViewById(R.id.current_track_artist_album);
-        albumArt = trackDetailsLayout.findViewById(R.id.current_track_album_art);
     }
 
     @Override
     public void onDataChange(DataSnapshot dataSnapshot) {
-        Log.d("INDEX_KEY", dataSnapshot.getKey());
-        Log.d("INDEX_VALUE", dataSnapshot.getValue().toString());
-
         int newIndex = dataSnapshot.getValue(Integer.class);
-        Log.d("INDEX_PARSED", ""+newIndex);
-        Log.d("SIZE", "" + adapter.getTracks().size());
         if (adapter.getTracks().isEmpty()) {
             return;
         }
@@ -56,6 +34,5 @@ public class PlayheadIndexChangeListener implements ValueEventListener {
 
     @Override
     public void onCancelled(DatabaseError databaseError) {
-
     }
 }
