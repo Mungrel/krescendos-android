ad8a3433513f19124c64054fc2fe27d8c918620b
Mungrel <nick.pollett@outlook.com>
2017-09-20 12:35:09 +1200

    Apply anim to create button click

--- app/src/main/java/com/krescendos/activities/JoinCreateActivity.java
+++ app/src/main/java/com/krescendos/activities/JoinCreateActivity.java
@@ -31,8 +31,8 @@ public class JoinCreateActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(JoinCreateActivity.this, CreateDetailsActivity.class);
-                intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
+                overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
             }
         });
     }
