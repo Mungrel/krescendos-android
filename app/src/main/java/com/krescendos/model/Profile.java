d99147b04a91d01a4d125b70fecfd8f1de5c9672
Mungrel <npol807@aucklanduni.ac.nz>
2017-09-29 20:57:19 +1300

    Add fields to Profile object

--- app/src/main/java/com/krescendos/model/Profile.java
+++ app/src/main/java/com/krescendos/model/Profile.java
@@ -1,4 +1,63 @@
 package com.krescendos.model;
 
 public class Profile {
+
+    private String birthDate;
+    private String country;
+    private String displayName;
+    private String email;
+    private String userID;
+    private String productType;
+
+    public boolean isPremiumUser() {
+        return productType.equals("premium");
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
 }
