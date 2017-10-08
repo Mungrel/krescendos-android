dfd4ecb83bd97dfecdaf4683b519836fb0d4d124
Mungrel <npol807@aucklanduni.ac.nz>
2017-10-08 17:07:40 +1300

    Implement appendUpNext method

--- app/src/main/java/com/krescendos/player/PlaylistAdapter.java
+++ app/src/main/java/com/krescendos/player/PlaylistAdapter.java
@@ -1,6 +1,7 @@
 package com.krescendos.player;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -10,9 +11,7 @@ import com.krescendos.model.Track;
 import com.krescendos.text.TextUtils;
 import com.krescendos.web.Requester;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 
@@ -24,19 +23,21 @@ public class PlaylistAdapter {
     private Queue<Track> tracks;
     private Track currentTrack;
     private Context context;
+    private LayoutInflater inflater;
     private LinearLayout upNextLayout;
     private LinearLayout currentTrackLayout;
 
     public PlaylistAdapter(Context context, LinearLayout upNextLayout, LinearLayout currentTrackLayout) {
         this.tracks = new LinkedList<>();
         this.context = context;
+        this.inflater = LayoutInflater.from(context);
         this.upNextLayout = upNextLayout;
         this.currentTrackLayout = currentTrackLayout;
     }
 
     public void appendTrack(Track track) {
         tracks.add(track);
-        updateUpNextLayout();
+        appendUpNextLayout(track);
     }
 
     public void poll() {
@@ -54,8 +55,18 @@ public class PlaylistAdapter {
         albumArt.setImageUrl(currentTrack.getAlbum().getLargestImage().getUrl(), Requester.getInstance(context).getImageLoader());
     }
 
-    private void updateUpNextLayout() {
-        
+    private void appendUpNextLayout(Track appendedTrack) {
+        LinearLayout listItem = (LinearLayout) inflater.inflate(R.layout.player_list_layout, upNextLayout);
+
+        TextView trackName = listItem.findViewById(R.id.up_next_track_name);
+        TextView artistAlbum = listItem.findViewById(R.id.up_next_artist_album);
+        NetworkImageView albumArt = listItem.findViewById(R.id.up_next_album_art);
+
+        trackName.setText(appendedTrack.getName());
+        artistAlbum.setText(TextUtils.join(appendedTrack.getArtists()));
+        albumArt.setImageUrl(appendedTrack.getAlbum().getSmallestImage().getUrl(), Requester.getInstance(context).getImageLoader());
+
+        upNextLayout.addView(listItem);
     }
 
 }
