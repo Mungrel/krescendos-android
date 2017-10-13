781095be1a7d334ffdba67903e8e7f08fedd7a41
Mungrel <npol807@aucklanduni.ac.nz>
2017-10-13 20:15:01 +1300

    Add VoteHandler object

---
+++ app/src/main/java/com/krescendos/vote/VoteHandler.java
@@ -0,0 +1,40 @@
+package com.krescendos.vote;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+
+public class VoteHandler implements Transaction.Handler {
+
+    private DatabaseReference voteCountRef;
+    private VoteDirection direction;
+
+    public VoteHandler(DatabaseReference voteCountRef, VoteDirection direction) {
+        this.voteCountRef = voteCountRef;
+        this.direction = direction;
+    }
+
+    @Override
+    public Transaction.Result doTransaction(MutableData mutableData) {
+        Long voteCount = mutableData.getValue(Long.class);
+        if (voteCount == null) {
+            return Transaction.success(mutableData);
+        }
+
+        if (direction == VoteDirection.UP) {
+            voteCount += 1;
+        } else if (direction == VoteDirection.DOWN){
+            voteCount -= 1;
+        }
+
+        mutableData.setValue(voteCount);
+        return Transaction.success(mutableData);
+    }
+
+    @Override
+    public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {
+
+    }
+}
