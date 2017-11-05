ec0dfb861bb73d245ec79287d9601190ac3a64ee
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-06 12:52:00 +1300

    Add alternateWasActive boolean to VoteTransaction

--- app/src/main/java/com/krescendos/vote/VoteTransaction.java
+++ app/src/main/java/com/krescendos/vote/VoteTransaction.java
@@ -8,9 +8,11 @@ import com.google.firebase.database.Transaction;
 public class VoteTransaction implements Transaction.Handler {
 
     private VoteDirection direction;
+    private boolean alternateWasActive;
 
-    public VoteTransaction(VoteDirection direction) {
+    public VoteTransaction(VoteDirection direction, boolean alternateWasActive) {
         this.direction = direction;
+        this.alternateWasActive = alternateWasActive;
     }
 
     @Override
@@ -20,10 +22,12 @@ public class VoteTransaction implements Transaction.Handler {
             return Transaction.success(mutableData);
         }
 
+        int delta = (alternateWasActive) ? 2 : 1;
+
         if (direction == VoteDirection.UP) {
-            voteCount -= 1;
+            voteCount -= delta;
         } else if (direction == VoteDirection.DOWN) {
-            voteCount += 1;
+            voteCount += delta;
         }
 
         mutableData.setValue(voteCount);
