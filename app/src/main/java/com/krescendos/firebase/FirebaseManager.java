60a20711fd91b347e461d75154c68361805489c8
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-05 20:00:21 +1300

    Remove unused imports

--- app/src/main/java/com/krescendos/firebase/FirebaseManager.java
+++ app/src/main/java/com/krescendos/firebase/FirebaseManager.java
@@ -1,23 +1,7 @@
 package com.krescendos.firebase;
 
-import android.provider.ContactsContract;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.GenericTypeIndicator;
-import com.google.firebase.database.MutableData;
-import com.google.firebase.database.Transaction;
-import com.google.firebase.database.ValueEventListener;
-import com.krescendos.firebase.FirebaseRefs;
 import com.krescendos.firebase.transactions.PollQueueTransaction;
-import com.krescendos.model.Track;
-import com.krescendos.model.VoteItem;
-
-import java.nio.channels.spi.AbstractSelectionKey;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 public class FirebaseManager {
 
