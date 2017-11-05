e3704b0b49efd74eac806c73f7e678037ff2e3ae
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-05 19:27:31 +1300

    Add some potentially useful refs to FirebaseRefs

--- app/src/main/java/com/krescendos/firebase/FirebaseRefs.java
+++ app/src/main/java/com/krescendos/firebase/FirebaseRefs.java
@@ -1,5 +1,7 @@
 package com.krescendos.firebase;
 
+import android.provider.ContactsContract;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -10,7 +12,16 @@ public class FirebaseRefs {
         return getPartyRef(partyId).child("playlist").child(itemKey).child("voteCount");
     }
 
+    public static DatabaseReference getCurrentlyPlayingRef(String partyId) {
+        return getPartyRef(partyId).child("currentlyPlaying");
+    }
+
+    public static DatabaseReference getPartyStateRef(String partyId) {
+        return getPartyRef(partyId).child("partyState");
+    }
+
     private static DatabaseReference getPartyRef(String partyId) {
         return ROOT.child("party").child(partyId);
     }
+
 }
