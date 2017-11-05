2bb8ec2d8d107cecbf9ec426fe7d12e79308cce7
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-05 20:07:18 +1300

    Implement updateState method

--- app/src/main/java/com/krescendos/firebase/FirebaseManager.java
+++ app/src/main/java/com/krescendos/firebase/FirebaseManager.java
@@ -2,6 +2,8 @@ package com.krescendos.firebase;
 
 import com.google.firebase.database.DatabaseReference;
 import com.krescendos.firebase.transactions.PollQueueTransaction;
+import com.krescendos.firebase.transactions.UpdateStateTransaction;
+import com.krescendos.model.PartyState;
 
 public class FirebaseManager {
 
@@ -10,4 +12,8 @@ public class FirebaseManager {
         FirebaseRefs.getPlaylistRef(partyId).runTransaction(new PollQueueTransaction(currentlyPlayingRef));
     }
 
+    public static void updatePlayState(String partyId, PartyState state) {
+        DatabaseReference playStateRef = FirebaseRefs.getPartyStateRef(partyId);
+        playStateRef.runTransaction(new UpdateStateTransaction(state));
+    }
 }
---
+++ app/src/main/java/com/krescendos/firebase/transactions/UpdateStateTransaction.java
@@ -0,0 +1,28 @@
+package com.krescendos.firebase.transactions;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+import com.krescendos.model.Party;
+import com.krescendos.model.PartyState;
+
+public class UpdateStateTransaction implements Transaction.Handler{
+
+    private PartyState partyState;
+
+    public UpdateStateTransaction(PartyState partyState) {
+        this.partyState = partyState;
+    }
+
+    @Override
+    public Transaction.Result doTransaction(MutableData mutableData) {
+        mutableData.setValue(partyState);
+        return Transaction.success(mutableData);
+    }
+
+    @Override
+    public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {
+
+    }
+}
