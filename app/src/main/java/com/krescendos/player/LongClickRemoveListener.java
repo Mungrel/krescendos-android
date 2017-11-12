06f8e9d1f1c5c11ecbc03a33071bc76d3d693443
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-12 15:26:56 +1300

    Make use of Remove dialog

--- app/src/main/java/com/krescendos/player/LongClickRemoveListener.java
+++ app/src/main/java/com/krescendos/player/LongClickRemoveListener.java
@@ -1,24 +1,34 @@
 package com.krescendos.player;
 
+import android.content.Context;
 import android.view.View;
 
+import com.krescendos.dialog.OnYesListener;
+import com.krescendos.dialog.RemoveTrackDialog;
 import com.krescendos.firebase.FirebaseManager;
 import com.krescendos.firebase.FirebaseRefs;
 
 public class LongClickRemoveListener implements View.OnLongClickListener {
 
+    private Context context;
     private String partyID;
     private String itemID;
 
-    public LongClickRemoveListener(String partyID, String itemID) {
+    public LongClickRemoveListener(Context context, String partyID, String itemID) {
+        this.context = context;
         this.partyID = partyID;
         this.itemID = itemID;
     }
 
     @Override
     public boolean onLongClick(View view) {
-
-        FirebaseManager.removeItem(partyID, itemID);
+        RemoveTrackDialog dialog = new RemoveTrackDialog(context, new OnYesListener() {
+            @Override
+            public void onYes() {
+                FirebaseManager.removeItem(partyID, itemID);
+            }
+        });
+        
         return true;
     }
 }
