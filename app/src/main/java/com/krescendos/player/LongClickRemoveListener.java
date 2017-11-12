4804d9e305dcd451af287901efb3895d260a6efc
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-12 15:05:43 +1300

    Implement LongClick remove item listener

---
+++ app/src/main/java/com/krescendos/player/LongClickRemoveListener.java
@@ -0,0 +1,23 @@
+package com.krescendos.player;
+
+import android.view.View;
+
+import com.krescendos.firebase.FirebaseManager;
+import com.krescendos.firebase.FirebaseRefs;
+
+public class LongClickRemoveListener implements View.OnLongClickListener {
+
+    private String partyID;
+    private String itemID;
+
+    public LongClickRemoveListener(String partyID, String itemID) {
+        this.partyID = partyID;
+        this.itemID = itemID;
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        FirebaseManager.removeItem(partyID, itemID);
+        return true;
+    }
+}
