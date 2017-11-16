93d7fbd7bae1c13e61974a4df562db8209d84207
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-17 10:36:28 +1300

    Add local persistence constants class

---
+++ app/src/main/java/com/krescendos/local/PersistenceConstants.java
@@ -0,0 +1,8 @@
+package com.krescendos.local;
+
+public class PersistenceConstants {
+
+    public static final String ID_PREFS = "host_id_prefs";
+    public static final String LAST_ID_HOSTED = "last_id_hosted";
+
+}
