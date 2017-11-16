b9a8b64aa28051e5b6d5a48c56557c83e28d370e
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-17 10:41:31 +1300

    Retrieve last hosted party ID

--- app/src/main/java/com/krescendos/activities/JoinCreateActivity.java
+++ app/src/main/java/com/krescendos/activities/JoinCreateActivity.java
@@ -1,12 +1,14 @@
 package com.krescendos.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.krescendos.R;
+import com.krescendos.local.PersistenceConstants;
 
 public class JoinCreateActivity extends AppCompatActivity {
 
@@ -35,6 +37,18 @@ public class JoinCreateActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
             }
         });
+
+        // Retrieve last hosted party code from disk
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(PersistenceConstants.ID_PREFS, 0);
+        String lasthostedID = pref.getString(PersistenceConstants.LAST_ID_HOSTED, "");
+
+        if (lasthostedID.isEmpty()) {
+            // Default value, no previously hosted parties
+
+        } else {
+            // Check if the party still exists on Firebase
+            
+        }
     }
 
 }
