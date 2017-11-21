7d4a0a0d38f998e5d0fd56d80e527e163bcb1498
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-21 16:29:03 +1300

    Add toolbar title

--- app/src/main/java/com/krescendos/activities/PlaylistActivity.java
+++ app/src/main/java/com/krescendos/activities/PlaylistActivity.java
@@ -2,8 +2,12 @@ package com.krescendos.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.krescendos.R;
+import com.krescendos.model.Party;
 
 public class PlaylistActivity extends AppCompatActivity {
 
@@ -11,5 +15,17 @@ public class PlaylistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.search_toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        String spotifyUsername = getIntent().getStringExtra("username");
+
+        TextView toolbarTitle = toolbar.findViewById(R.id.toolbar_text);
+        toolbarTitle.setText(String.format("%s%s", spotifyUsername, getString(R.string.playlists_title)));
+
     }
 }
