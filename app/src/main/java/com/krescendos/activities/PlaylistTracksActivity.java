9ef25f94fc50b827c7693f83969272da89f8048e
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-21 20:59:21 +1300

    Implement back button logic

--- app/src/main/java/com/krescendos/activities/PlaylistTracksActivity.java
+++ app/src/main/java/com/krescendos/activities/PlaylistTracksActivity.java
@@ -3,12 +3,15 @@ package com.krescendos.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.krescendos.R;
+import com.krescendos.input.Keyboard;
 import com.krescendos.model.Party;
 import com.krescendos.model.Playlist;
+import com.krescendos.search.SearchTrackListAdapter;
 
 public class PlaylistTracksActivity extends AppCompatActivity {
 
@@ -28,6 +31,18 @@ public class PlaylistTracksActivity extends AppCompatActivity {
         TextView toolbarTitle = toolbar.findViewById(R.id.toolbar_text);
         toolbarTitle.setText(playlist.getName());
 
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Keyboard.hide(PlaylistTracksActivity.this);
+                onBackPressed();
+                overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
