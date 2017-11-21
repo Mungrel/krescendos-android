8a0d1f231bf6476a8419519668b796efee74035e
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-21 15:09:07 +1300

    Make parse type a list

--- app/src/main/java/com/krescendos/web/requests/PlaylistRequest.java
+++ app/src/main/java/com/krescendos/web/requests/PlaylistRequest.java
@@ -6,12 +6,15 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonRequest;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.krescendos.model.Party;
 import com.krescendos.model.Playlist;
 import com.krescendos.web.DefaultErrorListener;
 import com.krescendos.web.LongTimeoutRetryPolicy;
 
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class PlaylistRequest extends JsonRequest<Playlist> {
     public PlaylistRequest(String url, Response.Listener<Playlist> listener) {
@@ -23,7 +26,8 @@ public class PlaylistRequest extends JsonRequest<Playlist> {
     protected Response<Playlist> parseNetworkResponse(NetworkResponse response) {
         try {
             String jsonString = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
-            Playlist playlist = new Gson().fromJson(jsonString, Playlist.class);
+            Type type = new TypeToken<List<Playlist>>() {}.getType();
+            Playlist playlist = new Gson().fromJson(jsonString, type);
             return Response.success(playlist, HttpHeaderParser.parseCacheHeaders(response));
         } catch (UnsupportedEncodingException e) {
             return Response.error(new ParseError(e));
