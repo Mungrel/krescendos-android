272ee7c1a0939ae7bd8d892ba6531c0a4abff5c0
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-21 15:03:53 +1300

    Remove unused requests, add PlaylistRequest, and Playlist model object

---
+++ app/src/main/java/com/krescendos/model/Playlist.java
@@ -0,0 +1,43 @@
+package com.krescendos.model;
+
+import java.util.List;
+
+public class Playlist {
+
+    private String id;
+    private String name;
+    private String owner;
+    private List<Track> tracks;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+}
--- app/src/main/java/com/krescendos/web/requests/PlaylistRequest.java
+++ app/src/main/java/com/krescendos/web/requests/PlaylistRequest.java
@@ -5,33 +5,28 @@ import com.android.volley.ParseError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonRequest;
+import com.google.gson.Gson;
+import com.krescendos.model.Party;
+import com.krescendos.model.Playlist;
 import com.krescendos.web.DefaultErrorListener;
-import com.krescendos.web.DefaultResponseListener;
 import com.krescendos.web.LongTimeoutRetryPolicy;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 
-public class AppendRequest extends JsonRequest<JSONObject> {
-
-    public AppendRequest(String url) {
-        super(Method.POST, url, null, new DefaultResponseListener(), new DefaultErrorListener());
+public class PlaylistRequest extends JsonRequest<Playlist> {
+    public PlaylistRequest(String url, Response.Listener<Playlist> listener) {
+        super(Method.PUT, url, null, listener, new DefaultErrorListener());
         setRetryPolicy(new LongTimeoutRetryPolicy());
     }
 
     @Override
-    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
-        JSONObject object = null;
+    protected Response<Playlist> parseNetworkResponse(NetworkResponse response) {
         try {
             String jsonString = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
-            object = new JSONObject(jsonString);
-        } catch (UnsupportedEncodingException | JSONException e) {
-            e.printStackTrace();
+            Playlist playlist = new Gson().fromJson(jsonString, Playlist.class);
+            return Response.success(playlist, HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
             return Response.error(new ParseError(e));
         }
-
-        return Response.success(object, HttpHeaderParser.parseCacheHeaders(response));
     }
 }
