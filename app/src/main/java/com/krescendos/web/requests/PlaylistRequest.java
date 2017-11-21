44e2c639813d8d36c77ff1bf1d8f163b46548e1d
Mungrel <npol807@aucklanduni.ac.nz>
2017-11-21 15:13:27 +1300

    Propagate list type, implement requester method

--- app/src/main/java/com/krescendos/web/requests/PlaylistRequest.java
+++ app/src/main/java/com/krescendos/web/requests/PlaylistRequest.java
@@ -16,19 +16,19 @@ import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.util.List;
 
-public class PlaylistRequest extends JsonRequest<Playlist> {
-    public PlaylistRequest(String url, Response.Listener<Playlist> listener) {
+public class PlaylistRequest extends JsonRequest<List<Playlist>> {
+    public PlaylistRequest(String url, Response.Listener<List<Playlist>> listener) {
         super(Method.PUT, url, null, listener, new DefaultErrorListener());
         setRetryPolicy(new LongTimeoutRetryPolicy());
     }
 
     @Override
-    protected Response<Playlist> parseNetworkResponse(NetworkResponse response) {
+    protected Response<List<Playlist>> parseNetworkResponse(NetworkResponse response) {
         try {
             String jsonString = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
             Type type = new TypeToken<List<Playlist>>() {}.getType();
-            Playlist playlist = new Gson().fromJson(jsonString, type);
-            return Response.success(playlist, HttpHeaderParser.parseCacheHeaders(response));
+            List<Playlist> playlists = new Gson().fromJson(jsonString, type);
+            return Response.success(playlists, HttpHeaderParser.parseCacheHeaders(response));
         } catch (UnsupportedEncodingException e) {
             return Response.error(new ParseError(e));
         }
