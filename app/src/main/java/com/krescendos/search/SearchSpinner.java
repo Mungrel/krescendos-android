322c537a798a9406db591e2ca5321e4d72dd1325
Mungrel <nick.pollett@outlook.com>
2018-01-18 18:09:53 +1300

    Add TextView to SearchSpinner class

--- app/src/main/java/com/krescendos/search/SearchSpinner.java
+++ app/src/main/java/com/krescendos/search/SearchSpinner.java
@@ -5,17 +5,20 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.krescendos.R;
 
 public class SearchSpinner {
 
     private ImageView imageView;
+    private TextView textView;
     private Animation animation;
     private boolean running;
 
-    public SearchSpinner(Context context, ImageView imageView) {
+    public SearchSpinner(Context context, ImageView imageView, TextView textView) {
         this.imageView = imageView;
+        this.textView = textView;
         this.animation = AnimationUtils.loadAnimation(context, R.anim.rotate_indefinitely);
         this.running = false;
     }
@@ -26,12 +29,18 @@ public class SearchSpinner {
         }
         imageView.setVisibility(View.VISIBLE);
         imageView.startAnimation(animation);
+
+        textView.setVisibility(View.VISIBLE);
+
         running = true;
     }
 
     public void hide() {
         imageView.clearAnimation();
         imageView.setVisibility(View.GONE);
+
+        textView.setVisibility(View.GONE);
+
         running = false;
     }
 }
